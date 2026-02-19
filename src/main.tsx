b12ea4e1cdd9e8e58445522149757211271385d5
Luis-Alvarezz <luisangelug10@gmail.com>
2026-02-18 22:26:38 -0600

    [MODIFY]: Se cambio background de body general de la APP

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='bg-gray-200'>
+      <App />
+    </div>
   </StrictMode>,
 )
