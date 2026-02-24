305ec285cdc4c10fcaeaa6d76ac57c04968c2518
Luis-Alvarezz <luisangelug10@gmail.com>
2026-02-23 20:37:16 -0600

    [MODIFY]: Type para informacion personal y herencia de atributos

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,24 @@
-export type Job = {
-  id: number,
+type WithId = {
+  id: number
+}
+type WithName = {
   name: string,
+}
+type WithDescription = {
   description: string,
+}
+type WithImage = {
   image: {
     webp: string,
     png: string
   }
+}
+
+
+export type Job = WithId & WithName & WithDescription & WithImage &{
+
+}
+
+export type InformationPersonal = WithId & WithName & WithDescription & WithImage & {
+
 }
